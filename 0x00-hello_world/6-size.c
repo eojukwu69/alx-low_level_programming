770624d861ab7bfa16b87f047a70d6ecf6fb6dcf
Ezechukwu Ojukwu <eojukwu69@gmail.com>
2022-09-08 17:37:59 -0700

    Files committed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
-int main() {
-    int intType;
-    float floatType;
-    long int longIntType;
-    long long int longlongType;
-    char charType;
-
-    printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-    printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
-    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-    
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int intType;
+float floatType;
+long int longIntType;
+long long int longlongType;
+char charType;
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
 }
