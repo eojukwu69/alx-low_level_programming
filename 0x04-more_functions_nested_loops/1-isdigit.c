1e1d2912aca07d2c9e036e26e78261495c27b97d
Ezechukwu Ojukwu <eojukwu69@gmail.com>
2022-09-15 15:15:57 -0700

    Files committed

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - entry point to the file
+ * @c: character to be checked
+ * Description: takes in a char int (c) and check if its ippercase
+ * Return: (1) on true and (0) on false
+ */
+int _isdigit(int c)
+{
+        int i = 0;
+
+        if (c >= '0' && c <= '9')
+        {
+        i = 1;
+        }
+        return (i);
+}
