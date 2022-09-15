da8b19aa830e4e6b857f234304127b4bd5ea4f11
Ezechukwu Ojukwu <eojukwu69@gmail.com>
2022-09-15 16:29:31 -0700

    Files committed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,36 +3,36 @@
 
 
 /**
- * putchars - function called
+ * printchar - function called
  * @var: value
  * Description: print from 0 - 9
  * Return: returns the result
  */
-void putchars(char var)
+void printchar(char var)
 {
 	_putchar(var);
 }
 
 /**
- * my_putchar - function called
+ * printint - function called
  * @var: value
  * Description: print from 0 - 9
  * Return: returns the result
  */
-void my_putchar(int var)
+void printint(int var)
 {
 
 if (var < 0)
 {
-putchars('-');
+printchar('-');
 var = var * -1;
 }
 
 
 if (var / 10)
-my_putchar(var / 10);
+printint(var / 10);
 
-putchars((var % 10) + '0');
+printchar((var % 10) + '0');
 }
 
 /**
@@ -50,8 +50,8 @@ void more_numbers(void)
 
 		for (j = 0; j <= 14; j++)
 		{
-		my_putchar(j);
+		printint(j);
 		}
-		putchars('\n');
+		printchar('\n');
 	}
 }
