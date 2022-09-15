e74cf225ee029e1fba9f907da3b06219737f7d65
Ezechukwu Ojukwu <eojukwu69@gmail.com>
2022-09-15 14:55:32 -0700

    Files committed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,15 @@
 #include <ctype.h>
 /**
- * main - entry point to the file
- * returns (1) on true and (0) on false
+ * _isupper - entry point to the file
+ * Parameter -
+ * Return: (1) on true and (0) on false
  */
 int _isupper(int c)
 {
+	int i = 0;
+	if(c >= 65 && c >= 90)
+	{
+	i = 1;
+	}
 	return (isUpper(c))
 }
