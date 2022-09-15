64a9ee6b68ac9e2026f5e2bb3568711406b2aaae
Ezechukwu Ojukwu <eojukwu69@gmail.com>
2022-09-15 15:30:08 -0700

    Files committed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function called
+ * @a: first value
+ * @b: second value
+ * Description: Multiplies two integers
+ * Return: returns the result
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
