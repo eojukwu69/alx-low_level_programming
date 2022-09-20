080075bf5a739f1acf6437447233d33233146218
Ezechukwu Ojukwu <eojukwu69@gmail.com>
2022-09-20 16:24:52 -0700

    Files committed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,15 @@
  */
 int _strlen(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        int count = 0;
+	int count = 0;
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                count += 1;
-        }
-        return (count);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count += 1;
+	}
+	return (count);
 }
 
 /**
