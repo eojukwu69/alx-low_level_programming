8df9bcf2ee9ea0487051075cb150c9dc46ebde0e
Ezechukwu Ojukwu <eojukwu69@gmail.com>
2022-09-20 17:26:21 -0700

    Files committed

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen - function call
+ * @s: value
+ * Return: value
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count += 1;
+	}
+	return (count);
+}
+
+/**
+ * puts_half - function call
+ * @str:  value
+ * Return: value
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	int length = _strlen(src);
+
+	if (length % 2 != 0)
+	{
+		i = (length / 1) / 2;
+	}
+	else
+	{
+		i = length / 2;
+	}
+
+	for (; i < length; i++)
+		_putchar(str[i]);
+
+}
