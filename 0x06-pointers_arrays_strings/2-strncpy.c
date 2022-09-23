44d07ec022b3c7fd671f52d58cd938e492d00a53
Ezechukwu Ojukwu <eojukwu69@gmail.com>
2022-09-22 19:00:33 -0700

    Files committed

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strlen - function call
+ * @s: value
+ * Return: value
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count += 1;
+	}
+	return (count);
+}
+
+/**
+ * _strncpy - function call
+ * @src:  value
+ * @dest:  value2
+ * @n: number of bytes
+ * Return: value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	int length = _strlen(src);
+
+	if (n < length)
+	{
+		length = n;
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[0];
+	}
+
+	return (dest);
+}
